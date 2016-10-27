00b909034ff016957c24b0ff09f5e590676cffb4
Olivier Jacques <olivier.jacques@hpe.com>
2016-10-27 15:49:21 +0000

    Fix #5: Monolithic UI jobs failing

--- vagrant/provision/roles/jenkins/files/backupAllJobs.sh
+++ vagrant/provision/roles/jenkins/files/backupAllJobs.sh
@@ -3,7 +3,7 @@ echo "Copy Jenkins Configuration"
 cp -f /var/lib/jenkins/config.xml /provision/roles/jenkins/files/config.xml
 cp -f /var/lib/jenkins/hudson.tasks.Maven.xml /provision/roles/jenkins/files/hudson.tasks.Maven.xml
 cp -f /var/lib/jenkins/jenkins.mvn.GlobalMavenConfig.xml /provision/roles/jenkins/files/jenkins.mvn.GlobalMavenConfig.xml
-cp -f /var/lib/jenkins/org.jenkinsci.plugins.xvfb.XvfbBuildWrapper.xml /provision/roles/jenkins/files/org.jenkinsci.plugins.xvfb.XvfbBuildWrapper.xml
+cp -f /var/lib/jenkins/org.jenkinsci.plugins.xvfb.Xvfb.xml /provision/roles/jenkins/files/org.jenkinsci.plugins.xvfb.Xvfb.xml
 cp -f /var/lib/jenkins/org.jenkins.ci.plugins.xframe_filter.XFrameFilterPageDecorator.xml /provision/roles/jenkins/files/org.jenkins.ci.plugins.xframe_filter.XFrameFilterPageDecorator.xml
 
 echo "Download Jenkins Cli"
