df0980e832ceca4e1531130705d50d792a0d6413
Olivier Jacques <olivier.jacques@hpe.com>
2016-12-14 14:20:15 +0100

    DNS Prefix support for Azure - default to team1

--- vagrant/azure_start.sh
+++ vagrant/azure_start.sh
@@ -2,4 +2,9 @@
 
 . ./.env
 
-vagrant up --provider=azure --no-parallel 2>&1 | ts | tee -a vagrant.log
+# Use DNS suffix set by env variable
+if [ -z "${LAB_SUFFIX}" ]; then
+    LAB_SUFFIX='team1'
+fi
+
+LAB_SUFFIX=${LAB_SUFFIX} vagrant up --provider=azure --no-parallel 2>&1 | ts | tee -a vagrant.log
