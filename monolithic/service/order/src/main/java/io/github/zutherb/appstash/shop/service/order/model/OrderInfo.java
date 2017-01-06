ecfbe7dfb3254ff09744decae9fcfbe3daa7cee8
Omar Chavez Orozco <omar.chavez.orozco@gmail.com>
2017-01-06 11:25:01 -0600

    Update OrderInfo.java

--- monolithic/service/order/src/main/java/io/github/zutherb/appstash/shop/service/order/model/OrderInfo.java
+++ monolithic/service/order/src/main/java/io/github/zutherb/appstash/shop/service/order/model/OrderInfo.java
@@ -74,7 +74,7 @@ public class OrderInfo implements Serializable {
         for (OrderItemInfo orderItemInfo : orderItems) {
             totalSum = totalSum.add(orderItemInfo.getTotalSum());
         }
-        return totalSum * (0.25/100);
+        return totalSum.multiply(0.25/100);
     }
 
     public BigDecimal getTotalSum() {
@@ -82,7 +82,7 @@ public class OrderInfo implements Serializable {
         for (OrderItemInfo orderItemInfo : orderItems) {
             totalSum = totalSum.add(orderItemInfo.getTotalSum());
         }
-        return totalSum - (totalSum * (0.25/100));
+        return totalSum - (totalSum.multiply(0.25/100));
     }
     
     
