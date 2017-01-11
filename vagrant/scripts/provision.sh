8b657f0513aadeb5a1df61342452e7af501fcf6c
Olivier Jacques <olivier.jacques@hpe.com>
2017-01-11 12:45:37 +0100

    Fix issue with Ansible install which needs to be >=2.2

--- vagrant/scripts/provision.sh
+++ vagrant/scripts/provision.sh
@@ -8,18 +8,16 @@ if grep -Fxq "with_proxy: true" vars/default.yml; then
    proxy_host=`awk '/http_proxy_host/ {printf "%s",$2;exit}' vars/default.yml`
    proxy_port=`awk '/http_proxy_port/ {printf "%s",$2;exit}' vars/default.yml`
    export http_proxy=http://${proxy_host}:${proxy_port}
+   export https_proxy=http://${proxy_host}:${proxy_port}
 fi
 
-if [ ! -f /usr/bin/ansible-playbook ]
-    then
-    sudo -E apt-get update
-    sudo -E apt-get install -y software-properties-common
-    sudo -E apt-key add ansible.key.txt
-    sudo -E apt-add-repository -y ppa:ansible/ansible
-    sudo -E apt-get update
-    sudo -E apt-get install -y --allow-unauthenticated ansible
-fi
-
+# Install or update ansible
+sudo -E apt-get update
+sudo -E apt-get install -y software-properties-common
+sudo -E apt-key add ansible.key.txt
+sudo -E apt-add-repository -y ppa:ansible/ansible
+sudo -E apt-get update
+sudo -E apt-get install -y --allow-unauthenticated ansible
 
 echo "RUNNING ansible-playbook -c local --inventory-file=hosts --extra-vars='ansible_ssh_user=vagrant' --user=vagrant " $@
 
