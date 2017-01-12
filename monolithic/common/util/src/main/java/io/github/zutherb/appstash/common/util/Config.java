ec174cccfb9a400fd0c15506d056a6f5d63b415c
Omar Chavez Orozco <omar.chavez.orozco@gmail.com>
2017-01-12 10:58:59 -0600

    Create Config.java

---
+++ monolithic/common/util/src/main/java/io/github/zutherb/appstash/common/util/Config.java
@@ -0,0 +1,37 @@
+package io.github.zutherb.appstash.common.util;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Config {
+	public static String getProperty(String property) {
+		Properties prop = new Properties();
+		InputStream input = null;
+		String result = "";
+
+		try {
+
+			input = new FileInputStream("config.properties");
+
+			// load a properties file
+			prop.load(input);
+
+			// get the property value and print it out
+			result = prop.getProperty(property);
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return result;
+	}
+}
