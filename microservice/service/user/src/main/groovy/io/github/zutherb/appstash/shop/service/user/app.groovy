22746bbd0389f9792c28beb4944c0434e3b7e3f7
Omar Chavez Orozco <omar.chavez.orozco@gmail.com>
2017-01-24 17:27:13 -0600

    Update app.groovy

--- microservice/service/user/src/main/groovy/io/github/zutherb/appstash/shop/service/user/app.groovy
+++ microservice/service/user/src/main/groovy/io/github/zutherb/appstash/shop/service/user/app.groovy
@@ -3,16 +3,14 @@ package io.github.zutherb.appstash.shop.service.user
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.context.ApplicationContext
-import io.github.zutherb.appstash.common.util.Config
 
 /**
  * @author zutherb
  */
 class Boot implements CommandLineRunner {
-
+    public boolean springBoot = false
     public static void main(String[] args) {
-        if (Config.getProperty("SPRING_BOOT")!=null && 
-            Config.getProperty("SPRING_BOOT").equalsIgnoreCase("TRUE")){        
+        if (springBoot){        
             SpringApplication.run(ApplicationConfiguration.class, args)
         }
     }
