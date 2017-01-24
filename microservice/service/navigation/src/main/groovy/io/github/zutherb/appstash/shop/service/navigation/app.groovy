b2ac1e98d989685e748795d29f6d355fae2e745d
Omar Chavez Orozco <omar.chavez.orozco@gmail.com>
2017-01-24 17:27:22 -0600

    Update app.groovy

--- microservice/service/navigation/src/main/groovy/io/github/zutherb/appstash/shop/service/navigation/app.groovy
+++ microservice/service/navigation/src/main/groovy/io/github/zutherb/appstash/shop/service/navigation/app.groovy
@@ -7,7 +7,7 @@ import org.springframework.boot.SpringApplication
  * @author zutherb
  */
 class Boot implements CommandLineRunner {
-    public boolean springBoot = false;
+    public boolean springBoot = false
     static void main(String[] args) {
         if (springBoot){        
             SpringApplication.run(ApplicationConfiguration.class, args)
