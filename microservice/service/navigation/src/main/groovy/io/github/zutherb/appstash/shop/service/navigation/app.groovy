222347c3f8ab3ac7f79b2d5a657b6e9fbe4e9c37
Omar Chavez Orozco <omar.chavez.orozco@gmail.com>
2017-01-24 17:03:13 -0600

    Update app.groovy

--- microservice/service/navigation/src/main/groovy/io/github/zutherb/appstash/shop/service/navigation/app.groovy
+++ microservice/service/navigation/src/main/groovy/io/github/zutherb/appstash/shop/service/navigation/app.groovy
@@ -2,6 +2,7 @@ package io.github.zutherb.appstash.shop.service.navigation
 
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
+import io.github.zutherb.appstash.common.util.Config
 
 /**
  * @author zutherb
@@ -9,7 +10,10 @@ import org.springframework.boot.SpringApplication
 class Boot implements CommandLineRunner {
 
     static void main(String[] args) {
-        SpringApplication.run(ApplicationConfiguration.class, args)
+        if (Config.getProperty("SPRING_BOOT")!=null && 
+            Config.getProperty("SPRING_BOOT").equalsIgnoreCase("TRUE")){        
+            SpringApplication.run(ApplicationConfiguration.class, args)
+        }
     }
 
     @Override
