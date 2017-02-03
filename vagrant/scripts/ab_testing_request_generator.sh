6566fecce289c01160f2751dcddd68aec88c5dcd
Christophe Larsonneur <christophe.larsonneur@hpe.com>
2017-02-03 09:48:49 +0100

    Add ab-testing test/prod choose usecase and less output.

--- vagrant/scripts/ab_testing_request_generator.sh
+++ vagrant/scripts/ab_testing_request_generator.sh
@@ -7,12 +7,35 @@ else
    SERVER="shop"
 fi
 
+declare -A FEATURES
+VERSIONS=(A B C D E F)
+
+function is_version()
+{
+ data="$1"
+ let iVersion=0
+ for i in "${FEATURES[@]}"
+ do
+   if [ "$data" = "$i" ]
+   then
+      printf ${VERSIONS[$iVersion]}
+      return
+   fi
+   let iVersion++
+ done
+ FEATURES[$iVersion]="$data"
+ printf .${VERSIONS[$iVersion]}
+}
+
+
 let iCount=0
 echo "Shooting 80 requests"
 while [ $iCount -lt 80 ]
 do
-  printf "."
-  curl http://${SERVER}.microservice.io/partials/cart.html > /dev/null
+  data="$(curl -s http://${SERVER}.microservice.io/partials/cart.html)"
+  is_version "$data"
   let iCount++
   sleep 0.5
 done
+echo "
+done"
