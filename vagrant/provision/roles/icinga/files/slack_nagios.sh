ccf4985eba3f17f95c497cd4d30b654c1a06a563
Olivier Jacques <olivier.jacques@hpe.com>
2017-03-08 18:20:20 +0100

    Add Flowdock support for Hubot

--- vagrant/provision/roles/icinga/files/slack_nagios.sh
+++ vagrant/provision/roles/icinga/files/slack_nagios.sh
@@ -12,6 +12,7 @@
 #Modify these variables for your environment
 MY_ICINGA_HOSTNAME="monitoring.microservice.io"
 HUBOT_HOOK="http://ci-node:8888/publish"
+HUBOT_ADAPTER=slack
 
 #Set the message icon based on Nagios service state
 if [ "$ICINGA_SERVICESTATE" = "CRITICAL" ]
@@ -36,12 +37,21 @@ else
 fi
 
 #Send message to Hubot pubsub
-curl -H "Content-Type: application/json" -X POST -d '{"event":"nagios","data":"'\
-'{\"text\":\"'"${ICON} ${ICINGA_SERVICEDISPLAYNAME}"' is in '"${ICINGA_SERVICESTATE}"' condition. More details <http://monitoring.microservice.io/cgi-bin/icinga/status.cgi?host='"${ICINGA_HOSTNAME}"'|here>\",'\
-'\"attachments\":'\
-'[{ \"title\": \"Details\", \"fields\":[{\"title\":\"Host\",\"value\":\"'${ICINGA_HOSTNAME}'\",\"short\":\"true\"},'\
-'{\"title\":\"Service\",\"value\":\"'"${ICINGA_SERVICEDISPLAYNAME}"'\",\"short\":\"true\"}'\
-'], \"text\": \"'"${ICINGA_SERVICEOUTPUT}"'\", \"color\":\"'"${COLOR}"'\" }'\
-']'\
-'}"}' \
-${HUBOT_HOOK}
+
+if [ "$HUBOT_ADAPTER" = "slack" ]
+then
+  curl -H "Content-Type: application/json" -X POST -d '{"event":"nagios","data":"'\
+  '{\"text\":\"'"${ICON} ${ICINGA_SERVICEDISPLAYNAME}"' is in '"${ICINGA_SERVICESTATE}"' condition. More details <http://monitoring.microservice.io/cgi-bin/icinga/status.cgi?host='"${ICINGA_HOSTNAME}"'|here>\",'\
+  '\"attachments\":'\
+  '[{ \"title\": \"Details\", \"fields\":[{\"title\":\"Host\",\"value\":\"'${ICINGA_HOSTNAME}'\",\"short\":\"true\"},'\
+  '{\"title\":\"Service\",\"value\":\"'"${ICINGA_SERVICEDISPLAYNAME}"'\",\"short\":\"true\"}'\
+  '], \"text\": \"'"${ICINGA_SERVICEOUTPUT}"'\", \"color\":\"'"${COLOR}"'\" }'\
+  ']'\
+  '}"}' \
+  ${HUBOT_HOOK}
+else
+  curl -H "Content-Type: application/json" -X POST -d '{"event":"nagios","data":"' ${ICON} ${ICINGA_SERVICEDISPLAYNAME} \
+  ' is in ${ICINGA_SERVICESTATE} condition. More details <http://monitoring.microservice.io/cgi-bin/icinga/status.cgi?host=${ICINGA_HOSTNAME}>' \
+  '}' \
+  ${HUBOT_HOOK}
+fi
