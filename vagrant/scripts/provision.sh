14d4b4817f92173662b9aa0064bdf9d347cc972f
Arne Luehrs <arne.luehrs@hpe.com>
2019-10-15 12:45:24 +0200

    Update ansible for appserver1 & dbserver

--- vagrant/scripts/provision.sh
+++ vagrant/scripts/provision.sh
@@ -18,14 +18,14 @@ if [ $1 = "--local" ]; then
 
   # Stick to branch in vars/default.yml
   github_branch=`awk '/github_branch/ {printf "%s",$2;exit}' /provision/vars/default.yml`
-  
+
   # Clean local tags if they changed on Github
   if [ github_branch != "master" ]; then
     github_tag=`echo ${github_branch} | awk -F "/" '/tags/ {print $3}'`
     git tag -d ${github_tag}
     git fetch origin --tags
   fi
-  
+
   git checkout ${github_branch}
 
   sudo rsync -aHAXvh --update --exclude 'vagrant/provision/vars/default.yml' vagrant/provision /
@@ -49,7 +49,7 @@ if ! command -v ansible >/dev/null 2>&1; then
   sudo -E apt-key add ansible.key.txt
   sudo -E apt-add-repository -y ppa:ansible/ansible
   sudo -E apt-get update
-  sudo -E apt-get install -y --allow-unauthenticated ansible software-properties-common
+  sudo -E apt-get install -y --allow-unauthenticated ansible software-properties-common python-apt
 fi
 
 echo "RUNNING ansible-playbook -c local --inventory-file=hosts --extra-vars='ansible_ssh_user=vagrant' --user=vagrant " $@
