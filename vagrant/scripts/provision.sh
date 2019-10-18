4f2f1501cd02bf532c251f742172e93aef5ec1e4
Arne Luehrs <arne.luehrs@hpe.com>
2019-10-18 19:14:57 +0200

    Bugfix for elasticstack and libvirt image
    Introduced a bug in elasticstack ansible script: Fixed
    
    Libvirt image peru/ubuntu has some behavioral quirks outside proxy environment
    (NFS not installing). Forced rsync for libvirt environment

--- vagrant/scripts/provision.sh
+++ vagrant/scripts/provision.sh
@@ -46,10 +46,11 @@ fi
 
 # Install or update ansible if not there
 if ! command -v ansible >/dev/null 2>&1; then
+  sudo -E apt-get update
+  sudo -E apt-get install -y --allow-unauthenticated software-properties-common python-apt
   sudo -E apt-key add ansible.key.txt
   sudo -E apt-add-repository -y ppa:ansible/ansible
-  sudo -E apt-get update
-  sudo -E apt-get install -y --allow-unauthenticated ansible software-properties-common python-apt
+  sudo -E apt-get install -y --allow-unauthenticated ansible
 fi
 
 echo "RUNNING ansible-playbook -c local --inventory-file=hosts --extra-vars='ansible_ssh_user=vagrant' --user=vagrant " $@
